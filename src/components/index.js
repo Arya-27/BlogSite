dca579bfcd67754af8339f0df4a0c5bb6e21649c
Arya <mishraarya528@gmail.com>
2024-05-24 15:15:55 +0530

    First Commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,42 +1,30 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBTn";
-import Button from "./Button";
-import Select from "./Select";
-import Input from "./Input";
+import LogoutBTn from "./Header/LogoutBTn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
-import Post from "../pages/Post";
+import Button from "./Button";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
-import Home from "../pages/Home";
-import AllPost from "../pages/AllPost";
-import AddPost from "../pages/AddPost";
-import EditPost from "../pages/EditPost";
-
+import Input from "./Input";
 export {
-  EditPost,
-  AllPost,
-  AddPost,
-  Post,
-  Home,
-  AuthLayout,
-  PostCard,
-  PostForm,
-  //Post,
-  Login,
-  Signup,
-  RTE,
-  Input,
-  Select,
-  Header,
-  Footer,
-  Container,
-  Logo,
-  LogoutBtn,
-  Button,
-};
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBTn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
