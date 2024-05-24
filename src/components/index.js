8ee922fa5cb5da89098015b056fdba72fe7ad862
Arya <mishraarya528@gmail.com>
2024-05-24 15:08:05 +0530

    First Commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,29 +1,42 @@
-import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutBTn from "./Header/LogoutBTn.jsx";
+import LogoutBtn from "./Header/LogoutBTn";
+import Button from "./Button";
+import Select from "./Select";
+import Input from "./Input";
+import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
-import Button from "./Button";
+import Post from "../pages/Post";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
-import Input from "./Input";
+import Home from "../pages/Home";
+import AllPost from "../pages/AllPost";
+import AddPost from "../pages/AddPost";
+import EditPost from "../pages/EditPost";
+
 export {
-    Header,
-    Footer,
-    Container,
-    Logo,
-    LogoutBTn,
-    RTE,
-    Signup,
-    Login,
-    Button,
-    PostForm,
-    PostCard,
-    AuthLayout, 
-    Input,
-    Select,
-}
+  EditPost,
+  AllPost,
+  AddPost,
+  Post,
+  Home,
+  AuthLayout,
+  PostCard,
+  PostForm,
+  //Post,
+  Login,
+  Signup,
+  RTE,
+  Input,
+  Select,
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+};
